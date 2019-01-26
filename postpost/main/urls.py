2959fe715eacb487074aaebdf5e22d0a146eb94a
Dmitriy Boger <kotvberloge@gmail.com>
2019-01-26 17:29:28 +0300

    Add redoc format

--- postpost/main/urls.py
+++ postpost/main/urls.py
@@ -22,10 +22,10 @@ from rest_framework import permissions
 
 schema_view = get_schema_view(
    openapi.Info(
-      title="postpost",
+      title='postpost',
       default_version='v0.1',
-      description="Default description",
-      license=openapi.License(name="MIT License"),
+      description='Default description',
+      license=openapi.License(name='MIT License'),
    ),
    public=True,
    permission_classes=(permissions.AllowAny,),
@@ -33,7 +33,8 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
-    path('', schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
