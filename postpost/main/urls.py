bd69f491c560b5015953dca097261a17b162ab8c
Dmitriy Boger <kotvberloge@gmail.com>
2019-01-26 17:22:26 +0300

    Fix quotes for flake8 rules

--- postpost/main/urls.py
+++ postpost/main/urls.py
@@ -33,7 +33,7 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
-    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui("swagger", cache_timeout=0), name="schema-swagger-ui"),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
