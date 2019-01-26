02736024742d204a70d6602b0761a6d099ef3dab
Dmitriy Boger <kotvberloge@gmail.com>
2019-01-26 17:03:33 +0300

    Add swagger
    Closes #10

--- postpost/main/urls.py
+++ postpost/main/urls.py
@@ -16,8 +16,24 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+from rest_framework import permissions
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="postpost",
+      default_version='v0.1',
+      description="Default description",
+      license=openapi.License(name="MIT License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('api/', include('api.urls')),
     path('admin/', admin.site.urls),
 ]
