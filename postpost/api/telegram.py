07f9d60e49066ca5ce72a864dc179d54ab703053
Vladimir Shkoda <vladimir.shkoda.51@gmail.com>
2019-02-13 00:02:00 +0300

    Remove getMe method and move chat_id to the client's init.

--- postpost/api/telegram.py
+++ postpost/api/telegram.py
@@ -7,35 +7,27 @@ def send_post_to_telegram_chat(token: str, chat_id: str, post: PlatformPost) -> 
     """
     Sends post to telegram chat.
     """
-    tg = TgAPI(token=token)
+    tg = TgAPI(token=token, chat_id=chat_id)
     # TODO: Add media according to PlatformPost changes
-    return tg.send_message(chat_id=chat_id, text=post.text_for_posting)
+    return tg.send_message(text=post.text_for_posting)
 
 
 class TgAPI(object):
     """
     Local mini client for Telegram API.
 
-    Its purpose it to share API token among the methods.
+    Its purpose it to share API token and chat id among the methods.
     """
 
-    def __init__(self, token: str):
+    def __init__(self, token: str, chat_id: str):
         """
         Init client.
         """
         self._url = 'https://api.telegram.org/bot{0}/'.format(token)
-
-    def get_me(self):
-        """
-        Returns basic information about the bot.
-
-        Can be used to test connection.
-        """
-        return requests.get(self._url + 'getMe').json()
+        self._chat_id = chat_id
 
     def send_message(
         self,
-        chat_id: str,
         text: str,
         disable_notification: bool = False,
         disable_web_page_preview: bool = True,
@@ -45,41 +37,36 @@ class TgAPI(object):
         """
         return self._request(
             'sendMessage',
-            chat_id=chat_id,
+            chat_id=self._chat_id,
             text=text,
             parse_mode='Markdown',
             disable_notification=disable_notification,
             disable_web_page_preview=disable_web_page_preview,
         )
 
-    def send_photo(self, chat_id: str, photo: str, disable_notification: bool = False) -> dict:
+    def send_photo(self, photo: str, disable_notification: bool = False) -> dict:
         """
         Sends the photo to the chat.
         """
         return self._request(
             'sendPhoto',
-            chat_id=chat_id,
+            chat_id=self._chat_id,
             photo=photo,
             disable_notification=disable_notification,
         )
 
-    def send_animation(
-        self,
-        chat_id: str,
-        animation: str,
-        disable_notification: bool,
-    ) -> dict:
+    def send_animation(self, animation: str, disable_notification: bool = False) -> dict:
         """
         Sends the animation to the chat.
         """
         return self._request(
             'sendAnimation',
-            chat_id=chat_id,
+            chat_id=self._chat_id,
             animation=animation,
             disable_notification=disable_notification,
         )
 
-    def _request(self, method_name: str, **kwargs):
+    def _request(self, method_name: str, **kwargs) -> dict:
         response = requests.post(self._url + method_name, json=kwargs)
         response.raise_for_status()
         return response.json()
