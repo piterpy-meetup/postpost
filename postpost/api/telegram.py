e15de6c21ab6c0d3d39f906119f05340d845c929
Vladimir Shkoda <vladimir.shkoda.51@gmail.com>
2019-02-14 00:28:48 +0300

    Add telegram gate. (#21)
    - Add a tiny tg api client.
    - Add a general function to post to telegram.
    - Change the function task to call the general function.

---
+++ postpost/api/telegram.py
@@ -0,0 +1,72 @@
+import requests
+
+from api.models import PlatformPost
+
+
+def send_post_to_telegram_chat(token: str, chat_id: str, post: PlatformPost) -> dict:
+    """
+    Sends post to telegram chat.
+    """
+    tg = TgAPI(token=token, chat_id=chat_id)
+    # TODO: Add media according to PlatformPost changes
+    return tg.send_message(text=post.text_for_posting)
+
+
+class TgAPI(object):
+    """
+    Local mini client for Telegram API.
+
+    Its purpose it to share API token and chat id among the methods.
+    """
+
+    def __init__(self, token: str, chat_id: str):
+        """
+        Init client.
+        """
+        self._url = 'https://api.telegram.org/bot{0}/'.format(token)
+        self._chat_id = chat_id
+
+    def send_message(
+        self,
+        text: str,
+        disable_notification: bool = False,
+        disable_web_page_preview: bool = True,
+    ) -> dict:
+        """
+        Sends the text message to the chat.
+        """
+        return self._request(
+            'sendMessage',
+            chat_id=self._chat_id,
+            text=text,
+            parse_mode='Markdown',
+            disable_notification=disable_notification,
+            disable_web_page_preview=disable_web_page_preview,
+        )
+
+    def send_photo(self, photo: str, disable_notification: bool = False) -> dict:
+        """
+        Sends the photo to the chat.
+        """
+        return self._request(
+            'sendPhoto',
+            chat_id=self._chat_id,
+            photo=photo,
+            disable_notification=disable_notification,
+        )
+
+    def send_animation(self, animation: str, disable_notification: bool = False) -> dict:
+        """
+        Sends the animation to the chat.
+        """
+        return self._request(
+            'sendAnimation',
+            chat_id=self._chat_id,
+            animation=animation,
+            disable_notification=disable_notification,
+        )
+
+    def _request(self, method_name: str, **kwargs) -> dict:
+        response = requests.post(self._url + method_name, json=kwargs)
+        response.raise_for_status()
+        return response.json()
