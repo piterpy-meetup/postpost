d2027e211bba4749093d2059a346e24ee3bd9a51
Vladimir Shkoda <vladimir.shkoda.51@gmail.com>
2019-02-19 13:44:07 +0300

    Refactoring.
    - Move gate API clients to own module.
    - Add function to download media.
    - Modify gate API clients to work with IO instead of link to media.

--- postpost/gates/telegram.py
+++ postpost/gates/telegram.py
@@ -1,3 +1,5 @@
+from typing import IO, Dict, Union
+
 import requests
 
 from api.models import PlatformPost
@@ -8,9 +10,9 @@ def send_post_to_telegram_chat(token: str, chat_id: str, post: PlatformPost) -> 
     """
     Sends post to telegram chat.
     """
-    tg = TgAPI(token=token, chat_id=chat_id)
+    tg = TgAPI(token=token)
     # TODO: Add media according to PlatformPost changes
-    return tg.send_message(text=post.text_for_posting)
+    return tg.send_message(chat_id=chat_id, text=post.text_for_posting)
 
 
 class TgAPI(object):
@@ -20,15 +22,15 @@ class TgAPI(object):
     Its purpose it to share API token and chat id among the methods.
     """
 
-    def __init__(self, token: str, chat_id: str):
+    def __init__(self, token: str):
         """
         Init client.
         """
         self._url = 'https://api.telegram.org/bot{0}/'.format(token)
-        self._chat_id = chat_id
 
     def send_message(
         self,
+        chat_id: str,
         text: str,
         disable_notification: bool = False,
         disable_web_page_preview: bool = True,
@@ -38,37 +40,54 @@ class TgAPI(object):
         """
         return self._request(
             'sendMessage',
-            chat_id=self._chat_id,
+            chat_id=chat_id,
             text=text,
             parse_mode='Markdown',
             disable_notification=disable_notification,
             disable_web_page_preview=disable_web_page_preview,
         )
 
-    def send_photo(self, photo: str, disable_notification: bool = False) -> JSON:
+    def send_photo(
+        self,
+        chat_id: str,
+        photo: IO[bytes],
+        disable_notification: bool = False,
+    ) -> JSON:
         """
         Sends the photo to the chat.
         """
         return self._request(
             'sendPhoto',
-            chat_id=self._chat_id,
-            photo=photo,
+            chat_id=chat_id,
+            media={'photo': photo},
             disable_notification=disable_notification,
         )
 
-    def send_animation(self, animation: str, disable_notification: bool = False) -> JSON:
+    def send_animation(
+        self,
+        chat_id: str,
+        animation: IO[bytes],
+        disable_notification: bool = False,
+    ) -> JSON:
         """
         Sends the animation to the chat.
         """
         return self._request(
             'sendAnimation',
-            chat_id=self._chat_id,
-            animation=animation,
+            chat_id=chat_id,
+            media={'animation': animation},
             disable_notification=disable_notification,
         )
 
-    def _request(self, method_name: str, **kwargs) -> JSON:
-        response = requests.post(self._url + method_name, json=kwargs)
+    def _request(
+        self,
+        method_name: str,
+        media: Union[Dict[str, IO[bytes]], None] = None,
+        **kwargs,
+    ) -> JSON:
+        """
+        Sends request to the Telegram API method with the given payload.
+        """
+        response = requests.post(self._url + method_name, data=kwargs, files=media)
         response.raise_for_status()
-        json: JSON = response.json()
-        return json
+        return response.json()
