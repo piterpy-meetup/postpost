74600f0ac67f1a6f6409f351b9da8ad1a05893f6
Sergey Sokolov <dmemory7@ya.ru>
2019-03-02 16:32:51 +0300

    Add docker-compose

--- postpost/main/settings.py
+++ postpost/main/settings.py
@@ -125,7 +125,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_BROKER_URL = 'redis://redis:6379'
+CELERY_RESULT_BACKEND = 'redis://redis:6379'
+CELERY_ACCEPT_CONTENT = ['application/json']
+CELERY_TASK_SERIALIZER = 'json'
+CELERY_RESULT_SERIALIZER = 'json'
 
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': (
